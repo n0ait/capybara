d80d5d1e3f6c5d22c9951bc29114b2ef26bdb2bb
Noé DUBOSQ <ndubosq@tetralink.fr>
2024-01-05 16:52:50 +0100

    Auth callbacks

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
-import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 import { db } from "./lib/db";
+import { getUserById } from "./data/users";
 import authConfig from "./auth.config";
+import NextAuth, { type DefaultSession} from "next-auth";
 
 export const {
   handlers: { GET, POST },
@@ -10,8 +12,27 @@ export const {
   signOut
 } = NextAuth({
   callbacks: {
+    async session({ session, token }){
+      if(token.sub && session.user){ //sub = id user
+        session.user.id = token.sub;
+      }
+
+      if(token.role && session.user){
+        session.user.role = token.role as UserRole;
+      }
+
+
+      return session;
+    },
+
     async jwt({ token }){
-      console.log({token})
+      if(!token.sub) return token; //sub = id user
+
+      const existingUser = await getUserById(token.sub);
+
+      if(!existingUser) return token;
+      token.role = existingUser.role;
+
       return token;
     }
   },
